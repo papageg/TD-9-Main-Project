76a8dc585debb2bd355504505a95485dbdb1d5f2
papageg <greggordon98@gmail.com>
2019-06-30 23:51:56 -0700

    still lost
    still lost

--- routes.js
+++ routes.js
@@ -2,45 +2,79 @@ const express = require('express');
 const router = express.Router();
 const User = require("./db/models/user").User;
 var auth = require('basic-auth')
-var compare = require('tsscmp')
+// var compare = require('tsscmp')
 
+const authenticateUser = (req, res, next) =>{
+    let message = null;
 
+    // Parse the user's credentials from the Authorization header.
+    const credentials = auth(req);
+  
+    // If the user's credentials are available...
+    if (credentials) {
+      // Attempt to retrieve the user from the data store
+      // by their username (i.e. the user's "key"
+      // from the Authorization header).
+      const user = users.find(u => u.username === credentials.name);
+  
+      // If a user was successfully retrieved from the data store...
+      if (user) {
+        // Use the bcryptjs npm package to compare the user's password
+        // (from the Authorization header) to the user's password
+        // that was retrieved from the data store.
+        const authenticated = bcryptjs
+          .compareSync(credentials.pass, user.password);
+  
+        // If the passwords match...
+        if (authenticated) {
+          console.log(`Authentication successful for username: ${user.username}`);
+  
+          // Then store the retrieved user object on the request object
+          // so any middleware functions that follow this middleware function
+          // will have access to the user's information.
+          req.currentUser = user;
+        } else {
+          message = `Authentication failure for username: ${user.username}`;
+        }
+      } else {
+        message = `User not found for username: ${credentials.name}`;
+      }
+    } else {
+      message = 'Auth header not found';
+    }
+    // If user authentication failed...
+  if (message) {
+    console.warn(message);
+
+    // Return a response with a 401 Unauthorized HTTP status code.
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    // Or if user authentication succeeded...
+    // Call the next() method.
+    next();
+  }
+  }
 ////////////////// USER ROUTES ///////////////////////////
 
-router.get('/users', (req, res)=>{
-        var credentials = auth(req)
-       
-        // Check credentials
-        // The "check" function will typically be against your user store
-        if (!credentials || !check(credentials.name, credentials.pass)) {
-          res.statusCode = 401
-          res.setHeader('WWW-Authenticate', 'Basic realm="example"')
-          res.end('Access denied')
-        } else {
-          res.end('Access granted')
-        }
-      
-       
-      // Basic function to validate credentials for example
-      function check (name, pass) {
-        var valid = true
-       
-        // Simple method to prevent short-circut and use timing-safe compare
-        valid = compare(name, req.body.emailAdress) && valid
-        valid = compare(pass, req.body.password) && valid
-       
-        return valid
-      }
+router.get('/users', authenticateUser, (req, res)=>{
+    const user = req.currentUser;
+
+    res.json({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        emailAdress: user.emailAdress,
+        password: user.password
+    });
 });
 
 router.post('/users', function (req, res){
     //Creates a user, sets the Location header 
     //to "/", and returns no content
     User.create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        emailAdress: req.body.emailAdress,
-        password: req.body.password
+        firstName: User.firstName,
+        lastName: User.lastName,
+        emailAdress: User.emailAdress,
+        password: User.password
       }).then(() => {
         res.location('/');
         res.status(201).end();
@@ -50,6 +84,30 @@ router.post('/users', function (req, res){
     });
 
 });
+
+// var credentials = auth(req)
+       
+// // Check credentials
+// // The "check" function will typically be against your user store
+// if (!credentials || !check(credentials.name, credentials.pass)) {
+//   res.statusCode = 401
+//   res.setHeader('WWW-Authenticate', 'Basic realm="example"')
+//   res.end('Access denied')
+// } else {
+//   res.end('Access granted')
+// }
+
+
+// // Basic function to validate credentials for example
+// function check (name, pass) {
+// var valid = true
+
+// // Simple method to prevent short-circut and use timing-safe compare
+// valid = compare(name, req.body.emailAdress) && valid
+// valid = compare(pass, req.body.password) && valid
+
+// return valid
+// }
 
 //////////////////////////////////////////////////////////
 ///////////////////// Course Routes //////////////////////
