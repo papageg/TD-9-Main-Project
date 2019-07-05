060c8d05fe00181989d7e88e7a0382f42a854841
papageg <greggordon98@gmail.com>
2019-07-05 14:58:28 -0700

    auth
    auth

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require("../models/user").User;
+// const User = require("../models/user").User;
 const { users } = require('../seed/data.json');
 
 ////////////////// USER ROUTES ///////////////////////////
